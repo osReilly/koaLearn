86d8f87c7df76030913c98ccbd979b8bc9331014
osReilly <ozreilly@163.com>
2018-11-08 17:50:32 +0800

    dd

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps : [{
     name: 'API',
-    script: 'app.js',
+    script: 'index.js',
 
     // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
     args: 'one two',
@@ -19,7 +19,7 @@ module.exports = {
 
   deploy : {
     production : {
-      user : 'node',
+      user : 'root',
       host : '116.62.244.133',
       ref  : 'origin/master',
       repo : 'git@github.com:osReilly/koaLearn.git',
@@ -28,3 +28,39 @@ module.exports = {
     }
   }
 };
+
+// module.exports = {
+//   apps: [{
+//     name: "app",
+//     script: "index.js"
+//   }],
+//   deploy: {
+//     // "production" is the environment name
+//     production: {
+//       // SSH key path, default to $HOME/.ssh
+//       key: "D:/nodeko.pem",
+//       // SSH user
+//       user: "root",
+//       // SSH host
+//       host: ["116.62.244.133"],
+//       // SSH options with no command-line flag, see 'man ssh'
+//       // can be either a single string or an array of strings
+//       ssh_options: "StrictHostKeyChecking=no",
+//       // GIT remote/branch
+//       ref: "origin/master",
+//       // GIT remote
+//       repo: "git@github.com:osReilly/koaLearn.git",
+//       // path in the server
+//       path: "/var/www/my-repository",
+//       // // Pre-setup command or path to a script on your local machine
+//       // pre-setup: "apt-get install git ; ls -la",
+//       // // Post-setup commands or path to a script on the host machine
+//       // // eg: placing configurations in the shared dir etc
+//       // post-setup: "ls -la",
+//       // // pre-deploy action
+//       // pre-deploy-local: "echo 'This is a local executed command'",
+//       // // post-deploy action
+//       // post-deploy: "npm install"
+//     }
+//   }
+// }
